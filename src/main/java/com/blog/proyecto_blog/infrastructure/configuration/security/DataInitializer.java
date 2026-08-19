af7755fd1c36f6bec3baa3a92d6133bbf04a5120
AldairAlburqueque <aldairalburqueque8@gmail.com>
2026-08-19 15:57:14 -0500

    Mejoramiento de la insercion de las categorias mediante el DataInitializer

--- src/main/java/com/blog/proyecto_blog/infrastructure/configuration/security/DataInitializer.java
+++ src/main/java/com/blog/proyecto_blog/infrastructure/configuration/security/DataInitializer.java
@@ -9,7 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,15 +26,12 @@ public class DataInitializer {
         };
     }
 
-
     public void createRoles() {
-
             if (!rolRepository.existsByRol("Admin")) {
                 RolEntity admin = new RolEntity();
                 admin.setRol("Admin");
                 rolRepository.save(admin);
             }
-
             if (!rolRepository.existsByRol("User")) {
                 RolEntity user = new RolEntity();
                 user.setRol("User");
@@ -43,22 +40,20 @@ public class DataInitializer {
     }
 
     public void createCategories() {
-
-        List<String> categories = List.of(
-                "Java",
-                "Spring Boot",
-                "React",
-                "Docker",
-                "JavaScript"
+        Map<String, String> categories = Map.of(
+                "Java", "Mundo Java, JVM y buenas prácticas.",
+                "Spring Boot", "Desarrollo de microservicios y APIs REST.",
+                "React", "Construcción de interfaces modernas con frontend.",
+                "Docker", "Contenedores, despliegue e infraestructura.",
+                "JavaScript", "Ecosistema web, Node.js y ES6+."
         );
-
-        for (String category : categories) {
-            if (!categoryRepository.existsByCategoria(category)) {
-                CategoryEntity  entity = new CategoryEntity();
-                entity.setCategoria(category);
+        categories.forEach((name, description) -> {
+            if (!categoryRepository.existsByCategoria(name)) {
+                CategoryEntity entity = new CategoryEntity();
+                entity.setCategoria(name);
+                entity.setDescription(description);
                 categoryRepository.save(entity);
             }
-        }
-
+        });
     }
 }
